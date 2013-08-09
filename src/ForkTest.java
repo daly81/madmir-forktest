c0b15fb7743a661437b100ab4ab7740b66c9e6eb
clydewbrown <clydewbrown@gmail.com>
2013-08-09 07:49:43 -0400

    Test

--- src/ForkTest.java
+++ src/ForkTest.java
@@ -2,7 +2,7 @@
 public class ForkTest {
 
 	public static void main(String[] args) {
-		//**Replace this comment with your name and today's date
+		//*Replace this comment with your name and today's date
 	}
 
 }
